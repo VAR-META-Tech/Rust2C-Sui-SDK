5941bc68ceaf0b6c30b3f5fca3c00262e52017cf
ngoc <ngochc1@gmail.com>
2024-05-30 22:16:59 +0700

    remove redundant test.c

--- test.c
+++ test.c
@@ -5,44 +5,6 @@
 int main()
 {
 
-    // int test_result = test();
-    // if (test_result == 0)
-    // {
-    //     printf("Demo test succeeded.\n");
-    // }
-    // else
-    // {
-    //     printf("Demo test failed.\n");
-    // }
-
-    // // Create a wallet
-    // Wallet *wallet = generate_wallet();
-
-    // const char *address = "0xfa2fa73cdb69a883dd40792dc092d7af46d5c88072835373afff26885978b2ea";
-
-    // Call the Rust function with the C string
-    // Wallet *wallet = get_wallet_from_address(address);
-    // Wallet *wallet = generate_and_add_key();
-    // // Print the wallet details
-    // if (wallet)
-    // {
-    //     printf("Wallet Address: %s\n", wallet->address);
-    //     printf("Mnemonic: %s\n", wallet->mnemonic);
-    //     printf("Public Base64 Key: %s\n", wallet->public_base64_key);
-    //     printf("Private Key: %s\n", wallet->private_key);
-    //     printf("Key Scheme: %s\n", wallet->key_scheme);
-
-    //     // Free the wallet
-    //     free_wallet(wallet);
-    // }
-    // else
-    // {
-    //     printf("Failed to create wallet.\n");
-    // }
-
-    // import_from_private_key("AMO4GD+YyWCFdKqh6aiE1rvm28wbI1jS53vHTHjNHHeZ");
-    // import_from_mnemonic("biology orbit beef water polar evidence faith lake load glance bomb visa");
-
     WalletList wallet_list = get_wallets();
 
     for (size_t i = 0; i < wallet_list.length; ++i)
@@ -57,27 +19,6 @@ int main()
     }
 
     free_wallet_list(wallet_list);
-
-    // Call the Rust function to get the string array
-    ResultCStringArray result = available_rpc_methods();
-
-    // Check if there was an error
-    if (result.error != NULL)
-    {
-        printf("Error: %s\n", result.error);
-        free_error_string(result.error);
-    }
-    else
-    {
-        // Print the strings
-        for (int i = 0; i < result.strings.len; i++)
-        {
-            printf("String %d: %s\n", i, result.strings.data[i]);
-        }
-
-        // Free the allocated string array memory
-        free_strings(result.strings);
-    }
 
     return 0;
 }
